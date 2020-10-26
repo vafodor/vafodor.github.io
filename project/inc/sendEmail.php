56e700b988d41682fa7901a350c3750595331036
Akhmedov <58439311+vafodor@users.noreply.github.com>
2020-10-26 12:09:14 +0500

    asdasdas
    dasdasdas

--- project/inc/sendEmail.php
+++ project/inc/sendEmail.php
@@ -1,7 +1,7 @@
-﻿<?php
+<?php
 
 // Replace this with your own email address
-$siteOwnersEmail = 'user@website.com';
+$siteOwnersEmail = 'adsnowden21@gmail.com';
 
 
 if($_POST) {
@@ -13,18 +13,18 @@ if($_POST) {
 
    // Check Name
 	if (strlen($name) < 2) {
-		$error['name'] = "Please enter your name.";
+		$error['name'] = "Iltimos, ismingizni kiriting.";
 	}
 	// Check Email
 	if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
-		$error['email'] = "Please enter a valid email address.";
+		$error['email'] = "Iltimos e-pochta manzilingizni kiriting.";
 	}
 	// Check Message
 	if (strlen($contact_message) < 15) {
-		$error['message'] = "Please enter your message. It should have at least 15 characters.";
+		$error['message'] = "Xabaringizni kiriting. U kamida 15 ta belgidan iborat bo'lishi kerak.";
 	}
    // Subject
-	if ($subject == '') { $subject = "Contact Form Submission"; }
+	if ($subject == '') { $subject = "Aloqa formasini yuborish"; }
 
 
    // Set Message
@@ -32,7 +32,7 @@ if($_POST) {
 	$message .= "Email address: " . $email . "<br />";
    $message .= "Message: <br />";
    $message .= $contact_message;
-   $message .= "<br /> ----- <br /> This email was sent from your site's contact form. <br />";
+   $message .= "<br /> ----- <br /> Ushbu elektron pochta saytingizning aloqa shaklidan yuborilgan. <br />";
 
    // Set From: header
    $from =  $name . " <" . $email . ">";
@@ -50,7 +50,7 @@ if($_POST) {
       $mail = mail($siteOwnersEmail, $subject, $message, $headers);
 
 		if ($mail) { echo "OK"; }
-      else { echo "Something went wrong. Please try again."; }
+      else { echo "Nimadir noto'g'ri bajarildi. Iltimos, yana bir bor urinib ko'ring."; }
 		
 	} # end if - no validation error
 
